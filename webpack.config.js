ddd819fd3020b1ac8076f4812ca19dd591362dc3
Valerie <cosmic.pony705@gmail.com>
2019-12-17 11:22:44 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
     entry: { main: './src/js/index.js' },
@@ -18,8 +19,12 @@ module.exports = {
                 exclude: /node_modules/
             },
             {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+                test: /\.css$/i,
+                use: [
+                    (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+                    'css-loader', 
+                    'postcss-loader'
+                ]
             },
             {
                 test: /\.(png|jpg|gif|ico|svg)$/,
@@ -50,7 +55,7 @@ module.exports = {
             assetNameRegExp: /\.css$/g,
             cssProcessor: require('cssnano'),
             cssProcessorPluginOptions: {
-                preset: ['default'],
+                    preset: ['default'],
             },
             canPrint: true
         }),
